6b80166729f11c81e176f70e83e439e37b189a14
caruiter <99144307+caruiter@users.noreply.github.com>
2023-04-27 15:52:38 -0400

    pickup drop system
    - added enemyDrops script
       - on destroy, instantiates coin pickups on unit circle
    - references coin prefab from playerInventory script

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     //this holds the player's inventory. currently all that we have in here is gold, but we may later add gun types and/or ammo
+    //it also holds the gold prefabs, for the moment
 
     public int gold;
+    public GameObject coinPrefab;
 
     // Start is called before the first frame update
     void Start()
